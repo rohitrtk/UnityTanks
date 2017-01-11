b12bc56308f558b1868b8656cc92fd950af1b4c8
rtk <rohit_rtk@hotmail.com>
2017-01-10 21:10:38 -0500

    Second Commit

---
+++ Assets/Scripts/PowerUpSpinner.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUpSpinner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(15, 38, 45) * Time.deltaTime);
+	}
+}
